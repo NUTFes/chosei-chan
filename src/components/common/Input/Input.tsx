5a387cb84b830ca12524693d6c83a96111935c33
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-22 21:11:34 +0900

    修正

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -2,7 +2,7 @@ import { InputProps } from './Input.types';
 import classNames from 'classnames';
 
 const Input: React.FC<InputProps> = (props) => {
-  return <input className={classNames('input input-bordered')} {...props} />;
+  return <input className="input input-bordered" {...props} />;
 };
 
 export default Input;
