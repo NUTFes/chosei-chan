ec9a41d106711c920d06a3daadd271171049461a
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-22 17:37:14 +0900

    inputコンポーネントの作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,12 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [require('daisyui')],
+  daisyui: {
+    darkTheme: false,
+  },
+};
