0324cfab7d680ffe021710474434e6c2ac2b825b
imaimai17468 <tosiki_801_life@outlook.jp>
2023-07-22 23:46:52 +0900

    [feat] eslint, prettierの設定を追加

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  "plugins": [require('prettier-plugin-tailwindcss')],
+  "semi": false,
+  "singleQuote": true,　
+  "printWidth": 90,　
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "jsxSingleQuote": true
+}
