0f661d09c11a7900a8bd8aa5007b6ff3ce5d13d7
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-23 03:03:30 +0900

    [fix]メモの空を除去

--- src/components/screen/MemoList/MemoList.tsx
+++ src/components/screen/MemoList/MemoList.tsx
@@ -1,12 +1,15 @@
 import { MemoListProps } from './MemoList.types'
 
 const MemoList: React.FC<MemoListProps> = ({ users }) => {
+  const filterUsers = users.filter((user) => {
+    return user.memo != '' && user.memo
+  })
   return (
     <div className='w-8/12 rounded-lg bg-white p-8'>
       <div className='grid grid-cols-4 gap-5'>
         <p className='mb-8 text-center text-3xl underline'>メモ欄</p>
         <div className='col-span-3'></div>
-        {users.map((user) => (
+        {filterUsers.map((user) => (
           <>
             <p className='text-center text-xl'>{user.name}</p>
             <p className='col-span-3 my-auto'>{user.memo}</p>
