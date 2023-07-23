1fc5d9d1f183ce828158c8b5b6590ae3722e8907
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-23 21:12:05 +0900

    メインページの外観作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
-import { Inter } from "next/font/google";
-import { MainLayout } from "@/components/layout/MainLayout";
-const inter = Inter({ subsets: ["latin"] });
-
+import { Button, Input } from '@/components/common'
+import { MainLayout } from '@/components/layout/MainLayout'
 
 export default function Home() {
-  return <MainLayout>hello</MainLayout>;
-};
+  return (
+    <MainLayout>
+      <div className='mx-auto my-10 flex w-80 flex-wrap justify-center gap-4'>
+        <p className='w-fit rounded-lg bg-primary p-2 text-xl'>STEP1</p>
+        <p className='my-auto flex-wrap text-xl underline'>イベント名を入力</p>
+        <Input></Input>
+      </div>
+      <div className='my-4 flex justify-center gap-4'>
+        <p className='w-fit rounded-lg bg-primary p-2 text-xl'>STEP2</p>
+        <p className='my-auto flex-wrap text-xl underline'>日程の入力</p>
+      </div>
+      <div className='m-auto h-96 w-1/2 rounded-lg bg-accent text-center'>カレンダー</div>
+      <div className='mb-10 mt-5 flex justify-center'>
+        <Button>作成</Button>
+      </div>
+    </MainLayout>
+  )
+}
