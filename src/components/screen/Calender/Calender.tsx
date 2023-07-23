6627e69936de9e557351384e9413bc0e6539d37d
harata-t <22.t.harata.nutfes@gmail.com>
2023-07-23 21:47:57 +0900

    [fix]命名と型定義の修正、props定義

--- src/components/screen/Calender/Calender.tsx
+++ src/components/screen/Calender/Calender.tsx
@@ -1,87 +1,106 @@
-import { format, getDate, getDay, addMonths, subMonths,getMonth , endOfWeek, eachDayOfInterval, eachWeekOfInterval, startOfMonth, endOfMonth } from 'date-fns';
-import { useState } from 'react';
+import {
+  format,
+  getDate,
+  getDay,
+  addMonths,
+  subMonths,
+  getMonth,
+  endOfWeek,
+  eachDayOfInterval,
+  eachWeekOfInterval,
+  startOfMonth,
+  endOfMonth,
+} from 'date-fns'
+import { useState } from 'react'
+import { CalenderProps } from './Calender.types'
 
-const Calendar: React.FC = () => {
-  const [targetDate, setTargetDate] = useState(new Date());
-  const [selectedDates, setSelectedDates] = useState<number[]>([]);
+const Calendar: React.FC<CalenderProps> = (onChange) => {
+  const [targetMonth, setTargetMonth] = useState(new Date())
+  const [selectedDates, setSelectedDates] = useState<number[]>([])
 
-  const getCalendarArray = (date: number | Date) => {
+  const getCalendarArray = (date: Date) => {
     const sundays = eachWeekOfInterval({
       start: startOfMonth(date),
-      end: endOfMonth(date)
+      end: endOfMonth(date),
     })
-    return sundays.map(sunday =>
-      eachDayOfInterval({ start: sunday, end: endOfWeek(sunday) })
-    );
-  };
-  const calendar = getCalendarArray(targetDate);
+    return sundays.map((sunday) =>
+      eachDayOfInterval({ start: sunday, end: endOfWeek(sunday) }),
+    )
+  }
+  const calendar = getCalendarArray(targetMonth)
 
   const handleDateClick = (date: Date) => {
-    const unixTime = date.getTime() / 1000;
-    setSelectedDates(prevDates => {
+    const unixTime = date.getTime()
+    setSelectedDates((prevDates) => {
       if (prevDates.includes(unixTime)) {
-        return prevDates.filter(time => time !== unixTime).sort();
+        return prevDates.filter((time) => time !== unixTime).sort()
       } else {
-        return [...prevDates, unixTime].sort();
+        return [...prevDates, unixTime].sort()
       }
-    });
-  };
+    })
+  }
 
   const handlePreviousMonth = () => {
-    setTargetDate(current => subMonths(current, 1));
-  };
+    setTargetMonth((current) => subMonths(current, 1))
+  }
 
   const handleNextMonth = () => {
-    setTargetDate(current => addMonths(current, 1));
-  };
+    setTargetMonth((current) => addMonths(current, 1))
+  }
 
   const handleSubmit = () => {
-    console.log(selectedDates);
-  };
+    console.log(selectedDates)
+  }
 
   return (
-    <div className="grid w-[40%] my-2 mx-auto">
-      <div className="my-2 text-3xl flex justify-center ">
-        <button className="mr-auto text-2xl " onClick={handlePreviousMonth}>＜ {getMonth(subMonths(targetDate, 1)) + 1}月</button>
-        {format(targetDate, 'y年M月')}
-        <button className='ml-auto text-2xl ' onClick={handleNextMonth}>{getMonth(addMonths(targetDate, 1)) + 1}月 ＞</button>
+    <div className='mx-auto my-2 grid w-2/5 '>
+      <div className='my-2 flex justify-center text-3xl '>
+        <button className='mr-auto text-2xl ' onClick={handlePreviousMonth}>
+          ＜ {getMonth(subMonths(targetMonth, 1)) + 1}月
+        </button>
+        {format(targetMonth, 'y年M月')}
+        <button className='ml-auto text-2xl ' onClick={handleNextMonth}>
+          {getMonth(addMonths(targetMonth, 1)) + 1}月 ＞
+        </button>
       </div>
       <table>
         <thead>
           <tr>
-            <th className="py-2">日</th>
-            <th className="py-2">月</th>
-            <th className="py-2">火</th>
-            <th className="py-2">水</th>
-            <th className="py-2">木</th>
-            <th className="py-2">金</th>
-            <th className="py-2">土</th>
+            {['日', '月', '火', '水', '木', '金', '土'].map((day) => (
+              <th key={day} className='py-2'>
+                {day}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
           {calendar.map((weekRow, rowNum) => (
             <tr key={rowNum}>
-              {weekRow.map(date => {
-                const unixTime = date.getTime() / 1000;
-                const isSelected = selectedDates.includes(unixTime);
+              {weekRow.map((date) => {
+                const unixTime = date.getTime()
+                const isSelected = selectedDates.includes(unixTime)
                 return (
-                  <td key={getDay(date)} className="text-center py-1">
+                  <td key={getDay(date)} className='py-1 text-center'>
                     <button
                       onClick={() => handleDateClick(date)}
-                      className={`btn btn-circle btn-outline ${isSelected ? 'btn-active' : ''}`}
+                      className={`btn btn-circle btn-outline ${
+                        isSelected ? 'btn-active' : ''
+                      }`}
                     >
                       {getDate(date)}
                     </button>
                   </td>
-                );
+                )
               })}
             </tr>
           ))}
         </tbody>
       </table>
-      <button onClick={handleSubmit} className="btn btn-primary mt-4">送信</button>
+      <button onClick={handleSubmit} className='btn btn-primary mt-4'>
+        送信
+      </button>
     </div>
   )
 }
 
-export default Calendar;
+export default Calendar
---
+++ src/components/screen/Calender/Calender.types.ts
@@ -0,0 +1,3 @@
+export type CalenderProps = {
+  onChange: (selectedDates: number[]) => void
+}
