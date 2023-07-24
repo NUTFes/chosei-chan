8161d8880e085321dec4babca8ce8ec752513b1f
harata-t <22.t.harata.nutfes@gmail.com>
2023-07-24 10:47:38 +0900

    storybookのtitle変更

--- src/components/screen/Calender/Calender.stories.tsx
+++ src/components/screen/Calender/Calender.stories.tsx
@@ -1,13 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import Calender from './Calender';
+import type { Meta, StoryObj } from '@storybook/react'
+import Calender from './Calender'
 
 const meta: Meta<typeof Calender> = {
-  title: 'common/Calender',
+  title: 'screen/Calender',
   component: Calender,
   tags: ['autodocs'],
-};
+}
 
-export default meta;
-type Story = StoryObj<typeof Calender>;
+export default meta
+type Story = StoryObj<typeof Calender>
 
-export const Defualt: Story = {};
+export const Defualt: Story = {}
