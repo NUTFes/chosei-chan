0982ce15f1b063cd0c169cabfc1b227e75138072
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-24 11:25:01 +0900

    メモ欄の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Input } from '@/components/common'
+import { TextArea } from '@/components/common/TextArea'
 import { MainLayout } from '@/components/layout/MainLayout'
 
 export default function Home() {
@@ -9,8 +10,13 @@ export default function Home() {
         <p className='my-auto text-xl underline'>イベント名を入力</p>
         <Input />
       </div>
-      <div className='my-4 flex justify-center gap-4'>
+      <div className='mx-auto my-10 flex w-80 flex-wrap justify-center gap-4'>
         <p className='w-fit rounded-lg bg-primary p-2 text-xl'>STEP2</p>
+        <p className='my-auto text-xl underline'>メモを入力</p>
+        <TextArea bordered={true} ghosted={false} />
+      </div>
+      <div className='my-4 flex justify-center gap-4'>
+        <p className='w-fit rounded-lg bg-primary p-2 text-xl'>STEP3</p>
         <p className='my-auto text-xl underline'>日程の入力</p>
       </div>
       <div className='m-auto h-96 w-1/2 rounded-lg bg-accent text-center'>カレンダー</div>
