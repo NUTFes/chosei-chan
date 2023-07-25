c0224f11348ae8e9d2212917325105ba7beae344
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-25 19:52:13 +0000

    [fix] フッターが一番下にくるように修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        top: {
+          screen: '100vh',
+        },
       },
     },
   },
