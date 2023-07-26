8e586e13dcc11c074921f9345e686b03753cadf7
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-26 13:23:19 +0000

    [fix] 上手くいかなかったのでtop-screenを削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,6 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        top: {
-          screen: '100vh',
-        },
       },
     },
   },
