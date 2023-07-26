b88cad5c8e4c275fbd2f56d7b96aa2e9004ffd08
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-26 22:08:26 +0900

    [feat]zのスキーマ追加

--- src/type/common.ts
+++ src/type/common.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+
 export interface Available {
   from: number
   to: number
@@ -16,3 +18,22 @@ export interface Schedule {
   memo?: string
   dates: number[]
 }
+
+export const availableSchema = z.object({
+  from: z.number(),
+  to: z.number(),
+})
+
+export const userSchema = z.object({
+  name: z.string(),
+  memo: z.string().nullable(),
+  availables: z.array(availableSchema).nullable(),
+})
+
+export const scheduleSchema = z.object({
+  id: z.string().nullable(),
+  name: z.string(),
+  memo: z.string().nullable(),
+  users: z.array(userSchema).nullable(),
+  dates: z.array(z.number()),
+})
