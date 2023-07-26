f229b549510cc404f3bef8e855628e545ac55e05
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-27 00:25:23 +0900

    [fix]クライアントサイドのデータ受け取り処理

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,4 +1,3 @@
-import type { NextPage } from 'next'
 import { SCHEDULE, USER, USERS } from '@/constant/data'
 import {
   addSchedule,
@@ -8,14 +7,28 @@ import {
   deleteSchedule,
 } from '@/repositories/schedule'
 import { addUser, updateUser, deleteUser } from '@/repositories/user'
+import { Schedule } from '@/type/common'
 
+interface Props {
+  schedule: Schedule
+}
+
+export async function getServerSideProps() {
+  const id = 'x9pSN8saCyjaNOfXApLC'
+  const scheduleRes = await getSchedule(id)
+  return {
+    props: {
+      schedule: scheduleRes,
+    },
+  }
+}
 const insert = async () => {
   const res = await addSchedule(SCHEDULE)
   alert(res)
 }
 
 const get = async () => {
-  const id = 'oP4bjUnfhQeUQCERmdaS'
+  const id = 'x9pSN8saCyjaNOfXApLC'
   const res = await getSchedule(id)
   alert(res)
 }
@@ -38,81 +51,88 @@ const deleteS = async () => {
 }
 
 const insertUser = async () => {
-  const id = 'tNp6BizMPL2uRok5TKZF'
-  const doc = await addUser(id, USER)
-  alert(doc)
+  const id = 'x9pSN8saCyjaNOfXApLC'
+  const res = await addUser(id, USER)
+  alert(res)
 }
 
 const updateU = async () => {
   const id = 'tNp6BizMPL2uRok5TKZF'
-
-  const doc = await updateUser(id, USERS, USER, 1)
-  alert(doc)
+  const res = await updateUser(id, USERS, USER, 1)
+  alert(res)
 }
 
 const deleteU = async () => {
   const id = 'tNp6BizMPL2uRok5TKZF'
-
-  const doc = await deleteUser(id, USER)
-  alert(doc)
+  const res = await deleteUser(id, USER)
+  alert(res)
 }
 
-const Home: NextPage = () => {
+const Home = (props: Props) => {
+  const { schedule } = props
   return (
-    <div className='flex items-center justify-center gap-5'>
-      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-        <button
-          className='mt-4 w-60 rounded-full bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700'
-          onClick={() => insert()}
-        >
-          Insert Schedule
-        </button>
-        <button
-          className='mt-4 w-60 rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700'
-          onClick={() => get()}
-        >
-          Get Schedule
-        </button>
-        <button
-          className='mt-4 w-60 rounded-full bg-primary px-4 py-2 font-bold text-white hover:bg-primary-focus'
-          onClick={() => getAll()}
-        >
-          Get All Schedules
-        </button>
-        <button
-          className='mt-4 w-60 rounded-full bg-secondary px-4 py-2 font-bold text-white hover:bg-secondary-focus'
-          onClick={() => update()}
-        >
-          Update Schedule
-        </button>
-        <button
-          className='mt-4 w-60 rounded-full bg-accent px-4 py-2 font-bold text-white hover:bg-accent-focus'
-          onClick={() => deleteS()}
-        >
-          Delete Schedule
-        </button>
+    <>
+      <div className='flex items-center justify-center gap-5'>
+        <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+          <button
+            className='mt-4 w-60 rounded-full bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700'
+            onClick={() => insert()}
+          >
+            Insert Schedule
+          </button>
+          <button
+            className='mt-4 w-60 rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700'
+            onClick={() => get()}
+          >
+            Get Schedule
+          </button>
+          <button
+            className='mt-4 w-60 rounded-full bg-primary px-4 py-2 font-bold text-white hover:bg-primary-focus'
+            onClick={() => getAll()}
+          >
+            Get All Schedules
+          </button>
+          <button
+            className='mt-4 w-60 rounded-full bg-secondary px-4 py-2 font-bold text-white hover:bg-secondary-focus'
+            onClick={() => update()}
+          >
+            Update Schedule
+          </button>
+          <button
+            className='mt-4 w-60 rounded-full bg-accent px-4 py-2 font-bold text-white hover:bg-accent-focus'
+            onClick={() => deleteS()}
+          >
+            Delete Schedule
+          </button>
+        </div>
+        <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+          <button
+            className='mt-4 w-60 rounded-full bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700'
+            onClick={() => insertUser()}
+          >
+            Insert User
+          </button>
+          <button
+            className='mt-4 w-60 rounded-full bg-secondary px-4 py-2 font-bold text-white hover:bg-secondary-focus'
+            onClick={() => updateU()}
+          >
+            Update User
+          </button>
+          <button
+            className='mt-4 w-60 rounded-full bg-accent px-4 py-2 font-bold text-white hover:bg-accent-focus'
+            onClick={() => deleteU()}
+          >
+            Delete User
+          </button>
+        </div>
       </div>
-      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-        <button
-          className='mt-4 w-60 rounded-full bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700'
-          onClick={() => insertUser()}
-        >
-          Insert User
-        </button>
-        <button
-          className='mt-4 w-60 rounded-full bg-secondary px-4 py-2 font-bold text-white hover:bg-secondary-focus'
-          onClick={() => updateU()}
-        >
-          Update User
-        </button>
-        <button
-          className='mt-4 w-60 rounded-full bg-accent px-4 py-2 font-bold text-white hover:bg-accent-focus'
-          onClick={() => deleteU()}
-        >
-          Delete User
-        </button>
-      </div>
-    </div>
+      {/* <div className='flex flex-col items-center justify-center'>
+        <p>データ</p>
+        <p>{schedule.name}</p>
+        <p>{schedule.memo}</p>
+        {schedule.users?.map((user) => <p key={user.name}>{user.name}</p>)}
+      </div> */}
+    </>
   )
 }
 
