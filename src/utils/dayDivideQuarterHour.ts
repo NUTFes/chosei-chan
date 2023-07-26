6ec1b0c924f4ce793d80a270e46fd0830e6a286a
imaimai17468 <tosiki_801_life@outlook.jp>
2023-07-26 23:25:20 +0900

    [feat] 日付を与えると15分ごとにunit timeを出力する関数を追加

---
+++ src/utils/dayDivideQuarterHour.ts
@@ -0,0 +1,13 @@
+export const dayDivideQuarterHour = (unixTime: number): number[] => {
+  const date = new Date(unixTime)
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const day = date.getDate()
+  const start = new Date(year, month, day, 0, 0, 0).getTime()
+  const end = new Date(year, month, day, 23, 59, 59).getTime()
+  const result = []
+  for (let i = start; i <= end; i += 900 * 1000) {
+    result.push(i)
+  }
+  return result
+}
