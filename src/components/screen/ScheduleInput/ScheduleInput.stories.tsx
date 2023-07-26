afd89cee182beb6c9ddf45e665303d32593524a0
imaimai17468 <tosiki_801_life@outlook.jp>
2023-07-27 04:01:45 +0900

    [feat] スケジュールを30分単位に修正

--- src/components/screen/ScheduleInput/ScheduleInput.stories.tsx
+++ src/components/screen/ScheduleInput/ScheduleInput.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import format from 'date-fns/format'
 import ScheduleInput from './ScheduleInput'
-import { SCHEDULE } from '@/constant/data'
+import { SCHEDULE, USERS } from '@/constant/data'
 
 const meta: Meta<typeof ScheduleInput> = {
   title: 'screen/ScheduleInput',
@@ -18,11 +17,20 @@ export const Default: Story = {
     onChange: (availableDates) => {
       if (!availableDates) return
       availableDates.forEach((date, index) => {
-        console.log(
-          index,
-          format(date.from, 'yyyy-MM-dd HH:mm'),
-          format(date.to, 'yyyy-MM-dd HH:mm'),
-        )
+        // console.log(index, date.from, date.to)
+      })
+    },
+  },
+}
+
+export const EditUser: Story = {
+  args: {
+    schedule: SCHEDULE,
+    editUser: USERS[0],
+    onChange: (availableDates) => {
+      if (!availableDates) return
+      availableDates.forEach((date, index) => {
+        // console.log(index, date.from, date.to)
       })
     },
   },
--- src/utils/dayDivideHalfHour.ts
+++ src/utils/dayDivideHalfHour.ts
@@ -6,7 +6,7 @@ export const dayDivideQuarterHour = (unixTime: number): number[] => {
   const start = new Date(year, month, day, 0, 0, 0).getTime()
   const end = new Date(year, month, day, 23, 59, 59).getTime()
   const result = []
-  for (let i = start; i <= end; i += 900 * 1000) {
+  for (let i = start; i <= end; i += 1800 * 1000) {
     result.push(i)
   }
   return result
