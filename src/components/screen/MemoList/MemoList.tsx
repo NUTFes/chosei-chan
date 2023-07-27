df59257de7a9d7f957440e2369fbc38f87972ea5
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-27 07:59:06 +0000

    [fix] sp版で表示数が多い時、横にスクロールさせる

--- src/components/screen/MemoList/MemoList.tsx
+++ src/components/screen/MemoList/MemoList.tsx
@@ -5,17 +5,17 @@ const MemoList: React.FC<MemoListProps> = ({ users }) => {
     return user.memo
   })
   return (
-    <div className='w-full gap-10 rounded-lg bg-white p-8'>
+    <div className='w-full gap-10 rounded-lg bg-white p-4 md:p-8'>
       <p className='text-3xl'>メモ欄</p>
       <div className='divider' />
       <div className='chat chat-start items-end gap-4'>
         {filterUsers.map((user) => (
           <>
-            <p className='ml-4 rounded-2xl bg-primary p-3 text-lg text-white'>
+            <p className='md:ml-4 rounded-2xl bg-primary p-3 text-lg text-white'>
               {user.name}
             </p>
             <div className='chat-bubble chat-bubble-secondary my-auto'>
-              <p className='text-xs md:text-sm break-words text-white'>{user.memo}</p>
+              <p className='text-xs md:text-sm overflow-auto break-keep whitespace-nowrap hidden-scrollbar text-white'>{user.memo}</p>
             </div>
           </>
         ))}
