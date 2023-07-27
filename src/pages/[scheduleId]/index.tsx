4eff7383a0f23fcf14e4e170cae360d31bde1d1b
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-28 02:19:49 +0900

    [feat]クエリからid取得

--- src/pages/[scheduleId]/index.tsx
+++ src/pages/[scheduleId]/index.tsx
@@ -1,16 +1,17 @@
+import { GetServerSideProps } from 'next'
 import { ShareButton } from '@/components/common/ShareButton'
 import { MainLayout } from '@/components/layout/MainLayout'
 import { MemoList } from '@/components/screen/MemoList'
 import { ScheduleDisplay } from '@/components/screen/ScheduleDisplay'
-import { getSchedule } from '@/repositories'
+import { getSchedule } from '@/repositories/schedule'
 import { Schedule } from '@/type/common'
 
 interface Props {
   schedule: Schedule
 }
 
-export async function getServerSideProps() {
-  const id = 'DKT0UR75aWbHdNBaqesU'
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = String(context.params?.scheduleId) || ''
   const scheduleRes = await getSchedule(id)
   return {
     props: {
@@ -32,24 +33,26 @@ export default function Home(props: Props) {
                   イベント名
                 </p>
               </div>
-              <p className='break-all text-xl'>{schedule.name}</p>
-              <p className='whitespace-nowrap text-sm'>参加{schedule.users?.length}人</p>
+              <p className='break-all text-2xl'>{schedule.name}</p>
+              <p className='whitespace-nowrap text-base'>
+                参加{schedule.users?.length}人
+              </p>
             </div>
             <div className='flex w-11/12 flex-col gap-4'>
               <div className='flex items-end gap-4 border-b-2 border-primary'>
                 <div className='bg-primary p-2'>
                   <p className='whitespace-nowrap text-xl font-bold text-white'>備考</p>
                 </div>
-                <p className='whitespace-pre-wrap break-all text-xl'>{schedule.memo}</p>
+                <p className='whitespace-pre-wrap break-all text-base'>{schedule.memo}</p>
               </div>
               <div className='flex flex-row-reverse'>
                 <ShareButton />
               </div>
-              <div className='mx-auto w-full items-center'>
+              <div className='mx-auto flex w-full items-center justify-center'>
                 <ScheduleDisplay schedule={schedule} />
               </div>
             </div>
-            <div className='flex items-center justify-center'>
+            <div className='flex w-full items-center justify-center'>
               <MemoList users={schedule.users} />
             </div>
           </div>
