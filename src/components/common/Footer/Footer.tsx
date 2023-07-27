8007324ec8e64096408ccd1d24a8e9ca2b2fc612
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-27 19:39:43 +0000

    [fix] 左右に寄せて表示できるようにした

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -5,11 +5,11 @@ const Footer: React.FC = () => {
   return (
     <footer className='footer bg-neutral p-1 md:p-4 text-neutral-content'>
       <div className='w-full grid-flow-col items-center'>
-        <Image src='/logo_images.png' width={24} height={24} alt='logo' />
-        <div className='text-xs md:text-base md:place-self-center'>
-          Copyright © 2023 - NUTMEG
+        <div className='flex gap-1 items-center'>
+          <Image src='/logo_images.png' width={24} height={24} alt='logo' />
+          <div className='text-xs md:text-base'>Copyright © 2023 - NUTMEG</div>
         </div>
-        <div className='flex gap-1 md:gap-4 md:place-self-center md:justify-self-end'>
+        <div className='flex gap-1 md:gap-4 place-self-center justify-self-end'>
           <a
             className='hover:scale-125'
             href='https://twitter.com/nutfes_nutmeg?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'
