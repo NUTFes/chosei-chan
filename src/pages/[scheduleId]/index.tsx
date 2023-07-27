c5a7e1c27b4b95870d3607549b7688cc907f62b2
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-27 05:15:09 +0000

    [fix] 文字サイズの修正、フッターとメモ欄の間に空きを作った

--- src/pages/[scheduleId]/index.tsx
+++ src/pages/[scheduleId]/index.tsx
@@ -8,34 +8,32 @@ export default function Home() {
     <MainLayout>
       <div className='min-h-screen flex flex-col'>
         <main className='flex-grow'>
-          <div className='mt-20 flex flex-col items-center justify-center gap-8'>
+          <div className='my-20 flex flex-col items-center justify-center gap-8'>
             <div className='flex w-11/12 items-end gap-4 border-b-2 border-primary'>
               <div className='bg-primary p-2'>
-                <p className='whitespace-nowrap text-sm font-bold text-white md:text-xl'>
+                <p className='whitespace-nowrap font-bold text-white text-xl'>
                   イベント名
                 </p>
               </div>
-              <p className='break-all text-sm md:text-xl'>{SCHEDULE.name}</p>
+              <p className='break-all text-xl'>{SCHEDULE.name}</p>
               <p className='whitespace-nowrap text-sm'>参加{USERS.length}人</p>
             </div>
-            <div className='flex w-11/12 flex-col'>
+            <div className='flex w-11/12 flex-col gap-4'>
               <div className='flex items-end gap-4 border-b-2 border-primary'>
                 <div className='bg-primary p-2'>
-                  <p className='whitespace-nowrap text-sm font-bold text-white md:text-xl'>
+                  <p className='whitespace-nowrap text-xl font-bold text-white'>
                     備考
                   </p>
                 </div>
-                <p className='whitespace-pre-wrap break-all text-sm md:text-xl'>
+                <p className='whitespace-pre-wrap break-all text-xl'>
                   {SCHEDULE.memo}
                 </p>
               </div>
-              <div className='my-auto gap-4'>
-                <div className='flex flex-row-reverse'>
-                  <ShareButton />
-                </div>
-                <div className='h-40 items-center justify-center rounded-lg bg-primary text-white'>
-                  スケジュール
-                </div>
+              <div className='flex flex-row-reverse'>
+                <ShareButton />
+              </div>
+              <div className='h-40 items-center justify-center rounded-lg bg-primary text-white'>
+                スケジュール
               </div>
             </div>
             <div className='flex w-full items-center justify-center'>
