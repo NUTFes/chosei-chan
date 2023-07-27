a1d24446c0d2be6c615c6a4ec232275a37bf8ede
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-27 04:46:05 +0000

    [fix] memolistのレスポンシブ対応

--- src/components/screen/MemoList/MemoList.tsx
+++ src/components/screen/MemoList/MemoList.tsx
@@ -5,17 +5,17 @@ const MemoList: React.FC<MemoListProps> = ({ users }) => {
     return user.memo
   })
   return (
-    <div className='w-2/3 gap-10 rounded-lg bg-white p-8'>
+    <div className='w-full gap-10 rounded-lg bg-white p-8'>
       <p className='text-3xl'>メモ欄</p>
       <div className='divider' />
-      <div className='chat chat-start gap-4'>
+      <div className='chat chat-start items-end gap-4'>
         {filterUsers.map((user) => (
           <>
             <p className='ml-4 rounded-2xl bg-primary p-3 text-lg text-white'>
               {user.name}
             </p>
             <div className='chat-bubble chat-bubble-secondary my-auto'>
-              <p className='truncate text-white'>{user.memo}</p>
+              <p className='text-xs md:text-sm break-words text-white'>{user.memo}</p>
             </div>
           </>
         ))}
