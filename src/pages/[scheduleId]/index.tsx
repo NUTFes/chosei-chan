eda59d5def6059912a7fb9ae871e35e34971fe53
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-28 05:21:57 +0900

    [fix]ボタンの位置のレスポンシブ対応

--- src/pages/[scheduleId]/index.tsx
+++ src/pages/[scheduleId]/index.tsx
@@ -58,7 +58,11 @@ export default function Home(props: Props) {
               </div>
               <div className='flex flex-row-reverse'>
                 <ShareButton />
-                <Button onClick={toCreate}>予定を追加</Button>
+                <div className='mr-auto md:mx-0'>
+                  <Button onClick={toCreate} size='md' variants='primary'>
+                    予定を追加
+                  </Button>
+                </div>
               </div>
               <div className='mx-auto flex w-full items-center justify-center'>
                 <ScheduleDisplay schedule={schedule} />
