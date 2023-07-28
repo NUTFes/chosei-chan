aef417a272b948be29fe64b9dba2c4b5a94b2995
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-29 08:08:52 +0900

    [fix]shadowをmdに

--- src/components/screen/MemoList/MemoList.tsx
+++ src/components/screen/MemoList/MemoList.tsx
@@ -8,7 +8,7 @@ const MemoList: React.FC<MemoListProps> = ({ users }) => {
     })
 
   return (
-    <div className='w-11/12 gap-10 rounded-lg border bg-white  p-4 drop-shadow md:w-4/5 md:p-8'>
+    <div className='w-11/12 gap-10 rounded-lg border bg-white  p-4 drop-shadow-md md:w-4/5 md:p-8'>
       <p className='text-xl'>コメント欄</p>
       <div className='divider' />
       <div className='chat chat-start items-end gap-4'>
