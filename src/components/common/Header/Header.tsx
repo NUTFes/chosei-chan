4ce8697f06ec2fcea7532bc3f9c5a57889f4a992
imaimai17468 <tosiki_801_life@outlook.jp>
2023-07-28 23:33:00 +0900

    [fix] ボタンとレイアウトの修正

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 const Header: React.FC = () => {
   return (
-    <header className='flex h-12 md:h-16 items-center p-1 md:p-2'>
+    <header className='bg-neutral flex items-center p-1 md:p-2'>
       <Link href='/'>
         <img src='/header_logo.svg' className='h-full' />
       </Link>
