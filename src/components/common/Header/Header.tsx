01370fca7ee355f1b2cb7494365e95eef517ffd2
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-29 08:23:16 +0900

    [fix]bg-color変更

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 const Header: React.FC = () => {
   return (
-    <header className='bg-neutral flex items-center p-1 md:p-2'>
-      <Link href='/'>
+    <header className='flex items-center bg-white p-1 md:p-2'>
+      <Link href='/' className='rounded-box bg-neutral p-1'>
         <img src='/header_logo.svg' className='h-full' />
       </Link>
     </header>
