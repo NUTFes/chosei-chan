fad9beeb29218399736ca0f64c14729d62994bf7
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-28 12:44:51 +0000

    [fix] ヘッダーをクリックしたらトップページに繊維するようにした

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,7 +1,9 @@
 const Header: React.FC = () => {
   return (
     <header className='flex h-12 md:h-16 items-center p-1 md:p-2'>
-      <img src='/header_logo.svg' className='h-full' />
+      <a href='/'>
+        <img src='/header_logo.svg' className='h-full' />
+      </a>
     </header>
   )
 }
