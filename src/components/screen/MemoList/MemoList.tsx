6f9e52087850daa5f3375987cea2a624089e7336
Kubosaka <s223301@stn.nagaokaut.ac.jp>
2023-07-28 16:02:54 +0900

    [fix]メモリストのsp版のwidth調節

--- src/components/screen/MemoList/MemoList.tsx
+++ src/components/screen/MemoList/MemoList.tsx
@@ -8,14 +8,14 @@ const MemoList: React.FC<MemoListProps> = ({ users }) => {
     })
 
   return (
-    <div className='w-full gap-10 rounded-lg bg-white p-4 md:w-4/5 md:p-8'>
+    <div className='w-11/12 gap-10 rounded-lg bg-white p-4 md:w-4/5 md:p-8'>
       <p className='text-xl'>メモ欄</p>
       <div className='divider' />
       <div className='chat chat-start items-end gap-4'>
         {filterUsers &&
           filterUsers.map((user) => (
             <>
-              <p className='rounded-2xl bg-primary p-3 text-lg text-white md:ml-4'>
+              <p className='rounded-2xl bg-primary p-2 text-lg text-white md:ml-4 md:p-3'>
                 {user.name}
               </p>
               <div className='chat-bubble chat-bubble-secondary my-auto'>
