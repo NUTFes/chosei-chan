54883988d0a6c0fe34ff009cd8787bcd069a14d0
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-28 12:55:46 +0000

    [fix] Linkタグを使ってページ遷移を行う

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 const Header: React.FC = () => {
   return (
     <header className='flex h-12 md:h-16 items-center p-1 md:p-2'>
-      <a href='/'>
+      <Link href='/'>
         <img src='/header_logo.svg' className='h-full' />
-      </a>
+      </Link>
     </header>
   )
 }
