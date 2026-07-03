a869f753496d681082f1e6a1b2b3ef6c8132da5d
hikahana <22.h.hanada.nutfes@gmail.com>
2026-07-03 17:14:41 +0900

    Fix prettier workflow formatting mismatch

--- src/components/screen/ScheduleDisplay/ScheduleDisplay.tsx
+++ src/components/screen/ScheduleDisplay/ScheduleDisplay.tsx
@@ -126,10 +126,11 @@ const ScheduleDisplay: React.FC<ScheduleDisplayProps> = ({ schedule }) => {
             <div>
               {newDates[index].map((time, dates_index) => {
                 const selectedUsers = schedule.users
-                  ? schedule.users.filter((user) =>
-                      user.availables?.some(
-                        (available) => time >= available.from && time < available.to,
-                      ),
+                  ? schedule.users.filter(
+                      (user) =>
+                        user.availables?.some(
+                          (available) => time >= available.from && time < available.to,
+                        ),
                     )
                   : []
 
